ac2260eb8e23c938d2387c33b60bf06e8dd09264
etype <etypegk@gmail.com>
2017-02-21 11:21:58 +0200

    Add files via upload

---
+++ P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x33, 0x32, 0x1A, 0xA1, 0x34, 0x4f, 0x4f, 0x4e, 0x35, 0x45, 0x01, 0x21, 0x0A, 0x11, 0x01, 0x11 } };
+}
